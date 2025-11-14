f246a06a4849e6776f3b45937ab293e985af209b
Gaëlle <gaellepoisson.95@gmail.com>
2025-11-14 15:01:27 +0100

    Add files via upload

---
+++ items.php
@@ -0,0 +1,11 @@
+<?php
+// On crée un tableau contenant la liste des éléments de la barre de navigation
+$items = array (
+  'Home',
+    'About',
+    'Services',
+    'Portfolio',
+    'Team',
+    'Contact'
+);
+  ?>
---
+++ socialmedia.php
@@ -0,0 +1,9 @@
+<?php
+// On crée un  tableau qui contient les noms des réseaux sociaux
+ $socials = array (
+    'twitter',
+    'facebook',
+    'instagram',
+    'linkedin'
+);
+?>
